6919abdb4382764e0736d4f845558ca4d2c22a64
Akunna Onyekachi <93290304+akunna1@users.noreply.github.com>
2024-08-10 10:48:32 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -10,7 +10,7 @@ import App from './App.jsx';
 import './index.css';
 import './styles/global.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(// This refers to the div with id 'root' in your public/index.html
+ReactDOM.createRoot(document.getElementById('root')).render(// This refers to the div with id 'root' in my public/index.html
   <React.StrictMode>
     <Router> {/* Wrapping App with BrowserRouter/ Router */}
       <App />
